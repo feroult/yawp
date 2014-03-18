b210309b53d69c56c642b8bd4c1b26fb3b698cfc
Fernando Ultremare <feroult@gmail.com>
2014-03-18 11:45:48 -0300

    tests

---
+++ src/test/java/endpoint/DatastoreServletTest.java
@@ -0,0 +1,75 @@
+package endpoint;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import endpoint.utils.GAETest;
+import endpoint.utils.JsonUtils;
+
+public class DatastoreServletTest extends GAETest {
+
+	private static final String SIMPLE_OBJECT_JSON = "{aInt : 1, aLong : 1, aDouble : 1.1, aBoolean : true, aDate : '2013/12/26 23:55:01', aString : object1, aList : [{aString : anotherObject1}]}";
+	private static final String SIMPLE_ARRAY_JSON = String.format("[%s, %s]", SIMPLE_OBJECT_JSON, SIMPLE_OBJECT_JSON);
+
+	private DatastoreServlet servlet;
+
+	@Before
+	public void before() {
+		servlet = new DatastoreServlet(SimpleObject.class);
+	}
+
+	@Test
+	public void testCreate() {
+		String json = servlet.execute("POST", "/api/simpleobjects", SIMPLE_OBJECT_JSON, null).getText();
+
+		SimpleObject object = JsonUtils.from(json, SimpleObject.class);
+
+		object.assertObject(1, 1l, 1.1, true, "2013/12/26 23:55:01", "object1");
+
+		assertEquals(1, object.getaList().size());
+		object.getaList().get(0).assertAnotherObject("anotherObject1");
+	}
+
+	@Test
+	public void testCreateArray() {
+		String json = servlet.execute("POST", "/api/simpleobjects", SIMPLE_ARRAY_JSON, null).getText();
+
+		List<SimpleObject> objects = JsonUtils.fromArray(json, SimpleObject.class);
+
+		objects.get(0).assertObject(1, 1l, 1.1, true, "2013/12/26 23:55:01", "object1");
+		objects.get(1).assertObject(1, 1l, 1.1, true, "2013/12/26 23:55:01", "object1");
+
+		assertEquals(1, objects.get(0).getaList().size());
+		objects.get(0).getaList().get(0).assertAnotherObject("anotherObject1");
+	}
+
+	@Test
+	public void testIndex() {
+		servlet.execute("POST", "/api/simpleobjects", SIMPLE_OBJECT_JSON, null);
+		servlet.execute("POST", "/api/simpleobjects", SIMPLE_OBJECT_JSON, null);
+
+		String json = servlet.execute("GET", "/api/simpleobjects", null, null).getText();
+
+		List<SimpleObject> objects = JsonUtils.fromArray(json, SimpleObject.class);
+
+		assertEquals(2, objects.size());
+		objects.get(0).assertObject(1, 1l, 1.1, true, "2013/12/26 23:55:01", "object1");
+		objects.get(1).assertObject(1, 1l, 1.1, true, "2013/12/26 23:55:01", "object1");
+	}
+
+	@Test
+	public void testCustomAction() {
+		String json = servlet.execute("POST", "/api/simpleobjects", SIMPLE_OBJECT_JSON, null).getText();
+
+		SimpleObject object = JsonUtils.from(json, SimpleObject.class);
+
+		json = servlet.execute("PUT", "/api/simpleobjects/" + object.getId() + "/active", null, null).getText();
+
+		object = JsonUtils.from(json, SimpleObject.class);
+		object.assertObject(1, 1l, 1.1, true, "2013/12/26 23:55:01", "i was changed in action");
+	}
+}

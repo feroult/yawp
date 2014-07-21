ca1fc12237148bb921187bfeae0679a14a5b30fe
Fernando Ultremare <feroult@gmail.com>
2014-07-21 17:12:05 -0300

    query two orders

---
+++ src/main/java/endpoint/DatastoreQueryOrder.java
@@ -0,0 +1,30 @@
+package endpoint;
+
+public class DatastoreQueryOrder {
+
+	private String property;
+	
+	private String direction;
+
+	public DatastoreQueryOrder(String property, String direction) {
+		this.property = property;
+		this.direction = direction;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+}

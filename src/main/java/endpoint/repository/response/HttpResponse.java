d53206a079fb1c10463d61833f39dc16dba8a192
feroult <feroult@gmail.com>
2014-08-30 12:40:02 -0300

    refactoring

--- src/main/java/endpoint/repository/response/HttpResponse.java
+++ src/main/java/endpoint/repository/response/HttpResponse.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+// TODO think about if repository layer needs to know about http stuff
 public abstract class HttpResponse {
 
 	public abstract String getText();

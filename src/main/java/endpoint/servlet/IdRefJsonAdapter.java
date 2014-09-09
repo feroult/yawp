1393a0ad39db7360d78899aab361d27d6245f79d
feroult <feroult@gmail.com>
2014-09-09 13:15:57 -0300

    idref parse

--- src/main/java/endpoint/servlet/IdRefJsonAdapter.java
+++ src/main/java/endpoint/servlet/IdRefJsonAdapter.java
@@ -1,6 +1,5 @@
 package endpoint.servlet;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
 import com.google.gson.JsonDeserializationContext;
@@ -13,10 +12,8 @@ import com.google.gson.JsonSerializer;
 
 import endpoint.repository.IdRef;
 import endpoint.repository.Repository;
-import endpoint.repository.annotations.Endpoint;
 
 // TODO move to servlet package
-// TODO evaluate if whole tree should be returned or just current id as is
 public class IdRefJsonAdapter implements JsonSerializer<IdRef<?>>, JsonDeserializer<IdRef<?>> {
 
 	private Repository r;
@@ -27,20 +24,13 @@ public class IdRefJsonAdapter implements JsonSerializer<IdRef<?>>, JsonDeseriali
 
 	@Override
 	public JsonElement serialize(IdRef<?> idRef, Type type, JsonSerializationContext ctx) {
-		Class<?> clazz = idRef.getClazz();
-		Endpoint endpointAnnotation = clazz.getAnnotation(Endpoint.class);
-		return new JsonPrimitive(endpointAnnotation.path() + '/' + idRef.asLong());
+		return new JsonPrimitive(idRef.toString());
 	}
 
 	@Override
 	public IdRef<?> deserialize(JsonElement json, Type type, JsonDeserializationContext ctx) throws JsonParseException {
-		String id = json.getAsJsonPrimitive().getAsString();
-		Long asLong = Long.valueOf(id.split("/")[2]);
-		return IdRef.create(r, getIdRefClazz(type), asLong);
-	}
-
-	private Class<?> getIdRefClazz(Type type) {
-		return (Class<?>) ((ParameterizedType) type).getActualTypeArguments()[0];
+		String path = json.getAsJsonPrimitive().getAsString();
+		return IdRef.parse(r, path);
 	}
 
 }

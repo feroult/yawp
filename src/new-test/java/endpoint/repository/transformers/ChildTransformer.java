884b84b4c9b9a63bbde723065a5427f0a46f762d
feroult <feroult@gmail.com>
2014-09-12 17:38:44 -0300

    tests

---
+++ src/new-test/java/endpoint/repository/transformers/ChildTransformer.java
@@ -0,0 +1,12 @@
+package endpoint.repository.transformers;
+
+import endpoint.repository.models.parents.Child;
+
+public class ChildTransformer extends Transformer<Child> {
+
+	public Child simple(Child child) {
+		child.setName("xpto");
+		return child;
+	}
+
+}

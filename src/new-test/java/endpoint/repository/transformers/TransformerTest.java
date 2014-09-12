4026a1f23059659b9229a8aeab8e280da037f6af
feroult <feroult@gmail.com>
2014-09-12 17:27:02 -0300

    tests

--- src/new-test/java/endpoint/repository/transformers/TransformerTest.java
+++ src/new-test/java/endpoint/repository/transformers/TransformerTest.java
@@ -2,6 +2,7 @@ package endpoint.repository.transformers;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -22,25 +23,16 @@ public class TransformerTest extends EndpointTestCase {
 		assertEquals("xpto", ((BasicObject) map.get("innerObject")).getStringValue());
 	}
 
-	// @Test
-	// public void testListResult() {
-	// r.save(new SimpleObject("xpto1", 1l));
-	// r.save(new SimpleObject("xpto1", 2l));
-	// r.save(new SimpleObject("xpto2", 1l));
-	// r.save(new SimpleObject("xpto2", 2l));
-	// r.save(new SimpleObject("xpto3", 1l));
-	// r.save(new SimpleObject("xpto3", 2l));
-	//
-	// List<Map<String, String>> list = r.query(SimpleObject.class).<Map<String,
-	// String>> transform("simple").sort("innerValue", "desc")
-	// .sort("innerObject", "aLong", "desc").list();
-	//
-	// assertEquals("xpto3", list.get(0).get("innerValue"));
-	// assertEquals("xpto3", list.get(1).get("innerValue"));
-	// assertEquals("xpto2", list.get(2).get("innerValue"));
-	// assertEquals("xpto2", list.get(3).get("innerValue"));
-	// assertEquals("xpto1", list.get(4).get("innerValue"));
-	// assertEquals("xpto1", list.get(5).get("innerValue"));
-	// }
+	@Test
+	public void testListResultWithSort() {
+		r.save(new BasicObject("xpto1"));
+		r.save(new BasicObject("xpto2"));
+
+		List<Map<String, Object>> list = r.query(BasicObject.class).<Map<String, Object>> transform("simple").sort("innerValue", "desc")
+				.list();
+
+		assertEquals("xpto2", list.get(0).get("innerValue"));
+		assertEquals("xpto1", list.get(1).get("innerValue"));
+	}
 
 }

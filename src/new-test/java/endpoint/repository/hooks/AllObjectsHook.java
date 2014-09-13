8408b912ad087fc6e921f4761ec3dd1e615401f3
feroult <feroult@gmail.com>
2014-09-12 22:26:31 -0300

    test

--- src/new-test/java/endpoint/repository/hooks/AllObjectsHook.java
+++ src/new-test/java/endpoint/repository/hooks/AllObjectsHook.java
@@ -1,12 +1,6 @@
 package endpoint.repository.hooks;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.apache.commons.beanutils.PropertyUtils;
-
-import endpoint.repository.models.parents.Child;
-import endpoint.repository.models.parents.Grandchild;
-import endpoint.repository.models.parents.Parent;
+import endpoint.repository.models.basic.HookedObject;
 
 public class AllObjectsHook extends Hook<Object> {
 
@@ -16,34 +10,15 @@ public class AllObjectsHook extends Hook<Object> {
 			return;
 		}
 
-		setName(object, "xpto");
-	}
-
-	private void setName(Object object, String name) {
-		try {
-			PropertyUtils.setSimpleProperty(object, "name", name);
-		} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		}
+		HookedObject hooked = (HookedObject) object;
+		hooked.setStringValue("xpto all objects");
 	}
 
 	private boolean isHookTest(Object object) {
-		if (!(Parent.class.isInstance(object) || Child.class.isInstance(object) || Grandchild.class.isInstance(object))) {
+		if (!HookedObject.class.isInstance(object)) {
 			return false;
 		}
-		String name = getName(object);
-		return object != null && name.equals("hook_test");
-	}
-
-	private String getName(Object object) {
-		try {
-			Object name = PropertyUtils.getProperty(object, "name");
-			if (object == null) {
-				return null;
-			}
-			return name.toString();
-		} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-			throw new RuntimeException(e);
-		}
+		HookedObject hooked = (HookedObject) object;
+		return hooked.getStringValue() != null && hooked.getStringValue().equals("all_objects");
 	}
 }
--- src/new-test/java/endpoint/repository/hooks/HookTest.java
+++ src/new-test/java/endpoint/repository/hooks/HookTest.java
@@ -6,56 +6,33 @@ import java.util.List;
 
 import org.junit.Test;
 
-import endpoint.repository.models.basic.BasicObject;
-import endpoint.repository.models.parents.Child;
-import endpoint.repository.models.parents.Grandchild;
-import endpoint.repository.models.parents.Parent;
+import endpoint.repository.models.basic.HookedObject;
 import endpoint.utils.EndpointTestCase;
 
 public class HookTest extends EndpointTestCase {
 
 	@Test
 	public void testAfterSave() {
-		BasicObject object = new BasicObject("hook_test");
-
+		HookedObject object = new HookedObject("after_save");
 		r.saveWithHooks(object);
-
-		assertEquals("xpto", object.getStringValue());
+		assertEquals("xpto after save", object.getStringValue());
 	}
 
 	@Test
 	public void testAllObjectsHook() {
-		Parent parent = new Parent("hook_test");
-		r.saveWithHooks(parent);
-
-		Child child = new Child("hook_test");
-		child.setParentId(parent.getId());
-		r.saveWithHooks(child);
-
-		Grandchild grandchild = new Grandchild("hook_test");
-		grandchild.setChildId(child.getId());
-		r.saveWithHooks(grandchild);
-
-		assertEquals("xpto", parent.getName());
-		assertEquals("xpto", child.getName());
-		assertEquals("xpto", grandchild.getName());
+		HookedObject object = new HookedObject("all_objects");
+		r.saveWithHooks(object);
+		assertEquals("xpto all objects", object.getStringValue());
 	}
 
 	@Test
 	public void testBeforeQuery() {
-		saveProduct("xpto");
-		saveProduct("abcd");
+		r.save(new HookedObject("xpto1"));
+		r.save(new HookedObject("xpto2"));
 
-		List<Product> products = r.queryWithHooks(Product.class).list();
-		assertEquals(1, products.size());
-		assertEquals("xpto", products.get(0).getName());
-	}
-
-	private Product saveProduct(String name) {
-		Product product = new Product();
-		product.setName(name);
-		r.saveWithHooks(product);
-		return product;
+		List<HookedObject> objects = r.queryWithHooks(HookedObject.class).list();
+		assertEquals(1, objects.size());
+		assertEquals("xpto1", objects.get(0).getStringValue());
 	}
 
 }

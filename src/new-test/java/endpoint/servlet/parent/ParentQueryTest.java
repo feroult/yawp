136012f51edd756a0c86aa49df2060ce984de1fa
feroult <feroult@gmail.com>
2014-09-14 19:57:19 -0300

    tests

---
+++ src/new-test/java/endpoint/servlet/parent/ParentQueryTest.java
@@ -0,0 +1,42 @@
+package endpoint.servlet.parent;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import endpoint.repository.models.parents.Parent;
+import endpoint.servlet.ServletTestCase;
+
+public class ParentQueryTest extends ServletTestCase {
+
+	@Test
+	public void testOverObject() {
+		Parent parent = saveParent("xpto");
+
+		String json = put(uri("/parents/%s/touched", parent));
+		Parent retrievedParent = from(json, Parent.class);
+
+		assertEquals("touched xpto", retrievedParent.getName());
+	}
+
+	@Test
+	public void testOverCollection() {
+		saveParent("xpto1");
+		saveParent("xpto2");
+
+		String json = put(uri("/parents/touched"));
+		List<Parent> parents = fromList(json, Parent.class);
+
+		assertEquals(2, parents.size());
+		assertEquals("touched xpto1", parents.get(0).getName());
+		assertEquals("touched xpto2", parents.get(1).getName());
+	}
+
+	private Parent saveParent(String name) {
+		Parent parent = new Parent(name);
+		r.save(parent);
+		return parent;
+	}
+}

b0d6559f813a54801489d26e1004047cb84d0a37
Guilherme Carreiro <karreiro@gmail.com>
2014-09-15 17:07:33 -0300

    query - test

--- src/test/webapp/js/yawp.js
+++ src/test/webapp/js/yawp.js
@@ -1,94 +1,106 @@
 (function($) {
 
-	var baseUrl = '/api';
+  var baseUrl = '/api';
 
-	function defaultAjax(type, options) {
-		var request = $.ajax({
-			type : type,
-			url : options.url,
-			data : options.data,
-			async : options.async,
-			contentType : 'application/json;charset=UTF-8',
-			dataType : 'json'
-		});
+  function defaultAjax(type, options) {
+    var request = $.ajax({
+      type : type,
+      url : options.url,
+      data : options.data,
+      async : options.async,
+      contentType : 'application/json;charset=UTF-8',
+      dataType : 'json'
+    });
 
-		return request;
-	}
+    return request;
+  }
 
-	function parseArgs(args) {
-		if (args.length == 1) {
-			var object = args[0];
-			return {
-				endpoint : object.id,
-				object : object
-			};
-		}
+  function parseArgs(args) {
+    if (args.length == 1) {
+      var object = args[0];
+      return {
+        endpoint : object.id,
+        object : object
+      };
+    }
 
-		return {
-			endpoint : args[0],
-			object : args[1]
-		};
-	}
+    return {
+      endpoint : args[0],
+      object : args[1]
+    };
+  }
 
-	function save() {
-		var args = parseArgs(arguments);
+  function save() {
+    var args = parseArgs(arguments);
 
-		var options = {
-			url : baseUrl + args.endpoint,
-			data : JSON.stringify(args.object),
-		};
+    var options = {
+      url : baseUrl + args.endpoint,
+      data : JSON.stringify(args.object),
+    };
 
-		return defaultAjax('POST', options);
-	}
+    return defaultAjax('POST', options);
+  }
 
-	function idRef(id) {
-		var options = {
-			url : baseUrl + id
-		};
+  function idRef(id) {
+    var options = {
+      url : baseUrl + id
+    };
 
-		function fetch(callback) {
-			return defaultAjax('GET', options).done(callback);
-		}
+    function fetch(callback) {
+      return defaultAjax('GET', options).done(callback);
+    }
 
-		function destroy(callback) {
-			return defaultAjax('DELETE', options).done(callback);
-		}
+    function destroy(callback) {
+      return defaultAjax('DELETE', options).done(callback);
+    }
 
-		return {
-			fetch : fetch,
-			destroy : destroy
-		}
-	}
+    return {
+      fetch : fetch,
+      destroy : destroy
+    }
+  }
 
-	function query(endpoint) {
-	  var	condition = {};
+  function query(endpoint) {
+    var q = {};
 
-		function where(data) {
-			condition.q = JSON.stringify({ where : data });
-			return this;
-		}
+    function where(data) {
+      q.where = data;
+      return this;
+    }
 
-		function list() {
-			var options = {
-				url : baseUrl + endpoint,
-				data : condition
-			};
+    function order(data) {
+      q.order = data;
+      return this;
+    }
 
-			return defaultAjax('GET', options);
-		}
+    function sort(data) {
+      q.sort = data;
+      return this;
+    }
 
-		return {
-			where: where,
-			list: list
-		};
-	}
+    function list(callback) {
+      var options = {
+        url : baseUrl + endpoint,
+        data : { q : JSON.stringify(q) }
+      };
 
-	var api = {
-		save : save,
-		idRef : idRef,
-		query : query
-	};
+      return defaultAjax('GET', options).done(callback);;
+    }
 
-	window.yawp = api;
+    return {
+      where: where,
+      order: order,
+      sort: sort,
+      list: list
+    };
+  }
+
+  var api = {
+    save : save,
+    idRef : idRef,
+    query : query
+  };
+
+  window.yawp = api;
 
 })(jQuery);
--- src/test/webapp/test/parent/yawp_grandchild_custom_action_test.js
+++ src/test/webapp/test/parent/yawp_grandchild_custom_action_test.js
@@ -1,21 +1,17 @@
-(function(t, yawp, fx) {
+// (function(t, yawp, fx) {
 
-  t.module('grandchild custom action');
+//   t.module('grandchild custom action');
 
-  t.testStart(function() {
-    fx.reset();
-  });
+//   t.testStart(function() {
+//     fx.reset();
+//   });
 
-  t.asyncTest('action over object', function(assert) {
-    // Grandchild grandchild = saveGrandchild("xpto", child);
-    // String json = put(uri("/parents/%s/children/%s/grandchildren/%s/touched", parent, child, grandchild));
-    // Grandchild retrievedGrandchild = from(json, Grandchild.class);
-    // assertEquals("touched xpto", retrievedGrandchild.getName());
-    // assertEquals(child.getId(), retrievedGrandchild.getChildId());
-  });
+//   t.asyncTest('action over object', function(assert) {
+//     // TODO
+//   });
 
-  t.asyncTest('action over collection', function(assert) {
-    // TODO
-  });
+//   t.asyncTest('action over collection', function(assert) {
+//     // TODO
+//   });
 
-})(QUnit, yawp, yawp.fixtures);
+// })(QUnit, yawp, yawp.fixtures);

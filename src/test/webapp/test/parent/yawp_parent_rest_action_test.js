f99ab7bd19a0760610120b407ee2a89ea8e04da5
feroult <feroult@gmail.com>
2014-09-15 10:54:46 -0300

    yawp tests

--- src/test/webapp/test/parent/yawp_parent_rest_action_test.js
+++ src/test/webapp/test/parent/yawp_parent_rest_action_test.js
@@ -6,32 +6,30 @@
 		fx.reset();
 	});
 
-	t.asyncTest("save new parent", function(assert) {
+	t.asyncTest("create", function(assert) {
 		expect(1);
 
-		var kurt = {
-			name : 'kurt'
-		};
-
-		yawp.save('/parents', kurt).done(function(retrievedKurt) {
+		yawp.save('/parents', {
+			name : 'xpto'
+		}).done(function(retrievedKurt) {
 			assert.equal(retrievedKurt.name, 'kurt');
 			t.start();
 		});
 	});
 
-	t.asyncTest("save existing parent", function(assert) {
-		expect(1);
-
-		var kurt = fx.parent('kurt', {
-			name : 'kurt'
-		});
-
-		kurt.name = 'changed kurt';
-
-		yawp.save(kurt.id, kurt).done(function(retrievedKurt) {
-			assert.equal(retrievedKurt.name, 'changed kurt');
-			t.stop();
-		});
-	});
+//	t.asyncTest("update", function(assert) {
+//		expect(1);
+//
+//		var kurt = fx.parent('kurt', {
+//			name : 'kurt'
+//		});
+//
+//		kurt.name = 'changed kurt';
+//
+//		yawp.save(kurt.id, kurt).done(function(retrievedKurt) {
+//			assert.equal(retrievedKurt.name, 'changed kurt');
+//			t.stop();
+//		});
+//	});
 
 })(QUnit, yawp, yawp.fixtures);

948a56617d6e0f523d16bcf22ab96ee1cc964ec9
feroult <feroult@gmail.com>
2014-09-18 17:42:22 -0300

    yawp js api

---
+++ src/test/webapp/test/child/yawp_child_query_test.js
@@ -0,0 +1,77 @@
+(function(t, yawp, fx) {
+
+	t.moduledef('child query', {
+		testStart : function() {
+			fx.reset();
+
+			fx.parent('parent1', {
+				name : 'xpto1'
+			});
+
+			fx.child('child1', {
+				name : 'xpto1',
+				parentId : fx.parent('parent1').id
+			});
+
+			fx.child('child2', {
+				name : 'xpto2',
+				parentId : fx.parent('parent1').id
+			});
+
+			fx.child('child3', {
+				name : 'xpto3',
+				parentId : fx.parent('parent1').id
+			});
+
+			fx.parent('parent2', {
+				name : 'xpto2'
+			});
+
+			fx.child('child4', {
+				name : 'xpto1',
+				parentId : fx.parent('parent2').id
+			});
+		}
+	});
+
+	t.asyncTest('where', function(assert) {
+		expect(2);
+
+		var parent1 = fx.parent('parent1');
+
+		var where = [ 'name', '=', 'xpto1' ];
+
+		yawp('/children').from(parent1).where(where).list(function(children) {
+			assert.equal(children.length, 1);
+			assert.equal(children[0].name, 'xpto1');
+			t.start();
+		});
+	});
+
+	t.asyncTest('global', function(assert) {
+		expect(9);
+
+		var parent1 = fx.parent('parent1');
+		var parent2 = fx.parent('parent2');
+
+		var order = [ {
+			p : 'name'
+		} ];
+
+		yawp('/children').order(order).list(function(children) {
+			assert.equal(children.length, 4);
+
+			assert.equal(children[0].name, 'xpto1');
+			assert.equal(children[1].name, 'xpto1');
+			assert.equal(children[2].name, 'xpto2');
+			assert.equal(children[3].name, 'xpto3');
+
+			assert.ok(children[0].parentId == parent1.id || children[0].parentId == parent2.id);
+			assert.ok(children[1].parentId == parent1.id || children[1].parentId == parent2.id);
+			assert.equal(children[2].parentId, parent1.id);
+			assert.equal(children[3].parentId, parent1.id);
+			t.start();
+		});
+	});
+
+})(QUnit, yawp, yawp.fixtures);

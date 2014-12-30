67aed798558d3a78cf19719525ce2f8c757fc118
Luan Nico <luanpotter28@hotmail.com>
2014-12-30 18:38:44 -0200

    Fixing shippable to run js tests as well

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-MAVEN_OPTS=-Djava.awt.headless=true
-DATASTORE_FILE=../target/integration-tests-db.bin
-JVM_FLAGS="-Dappengine.jvmFlags.datastore.default_high_rep_job_policy_unapplied_job_pct=0"
-DEVSERVER_ARGS="-Djava.awt.headless=true -Dmaven.test.skip=true $JVM_FLAGS -Dappengine.port=8081 -Ddatastore.backing_store=$DATASTORE_FILE"
+if [ "$JAVA_HOME" = "/usr/lib/jvm/java-7-oracle/bin/java" ]; then
+  echo "Shippable bug detected!"
+  export JAVA_HOME="/usr/lib/jvm/java-7-oracle"
+fi
 
-rm -f $DATASTORE_FILE
+MAVEN_ARGS="-Dmaven.test.skip=true -Dappengine.port=8081 -Dappengine.jvmFlags.datastore.default_high_rep_job_policy_unapplied_job_pct=0"
 
-echo "starting devserver... done"
-(cd ..; mvn appengine:devserver_stop $DEVSERVER_ARGS)
-(cd ..; mvn appengine:devserver_start $DEVSERVER_ARGS)
-echo ' done'
+echo "starting devserver..."
+(cd ..; mvn appengine:devserver_start $MAVEN_ARGS)
+echo "done."
 
 ./runner.py ../src/test/webapp/test/all.html
 STATUS=$?
 
-(cd ..; mvn appengine:devserver_stop $DEVSERVER_ARGS)
-echo "stopping devserver... done"
+echo "stopping devserver..."
+(cd ..; mvn appengine:devserver_stop $MAVEN_ARGS)
+echo "done."
 
 exit $STATUS

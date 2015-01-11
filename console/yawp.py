b0b57dba6e8728056f685e827bd1a02052e5357b
feroult <feroult@gmail.com>
2015-01-11 03:40:32 -0200

    expando

--- console/yawp.py
+++ console/yawp.py
@@ -2,11 +2,13 @@ from google.appengine.ext import ndb
 
 # import yawp from yawp
 # yawp('/people').query().fetch()
+# http://stackoverflow.com/questions/10709893/ndb-expando-model-with-dynamic-textproperty
+# use expando
 
 def yawp(path):
     def _get_kind(cls):
         return path
 
-    Model = type(path, (ndb.Model,), {})
+    Model = type(path, (ndb.Expando,), {})
     Model._get_kind = classmethod(_get_kind)
     return Model

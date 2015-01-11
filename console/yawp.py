a1774d5b8af16ade1cd7e373fdf94e55f9493bd3
feroult <feroult@gmail.com>
2015-01-11 13:11:47 -0200

    expando

--- console/yawp.py
+++ console/yawp.py
@@ -3,7 +3,7 @@ from google.appengine.ext import ndb
 # import yawp from yawp
 # yawp('/people').query().fetch()
 # http://stackoverflow.com/questions/10709893/ndb-expando-model-with-dynamic-textproperty
-# use expando
+# http://stackoverflow.com/questions/19842671/migrating-data-when-changing-an-ndb-fields-property-type/19848970#19848970
 
 def yawp(path):
     def _get_kind(cls):

fa4fcd1f1ac7e8b022669e6ab675375c46b93c74
feroult <feroult@gmail.com>
2015-01-15 09:42:05 -0200

    console

--- console/yawp.py
+++ console/yawp.py
@@ -10,10 +10,46 @@ from functools import partial
 # http://stackoverflow.com/questions/19842671/migrating-data-when-changing-an-ndb-fields-property-type/19848970#19848970
 
 def change_property_to_text(key, entity):
+    print('.')
     entity[key] = datastore_types.Text(entity[key])
+
+def get_entities(new_keys):
+    keys = [key.to_old_key() for key in new_keys]
+    rpc = datastore.GetRpcFromKwargs({})
+    keys, multiple = datastore.NormalizeAndTypeCheckKeys(keys)
+    entities = None
+    try:
+        entities = datastore.Get(keys, rpc=rpc)
+    except datastore_errors.EntityNotFoundError:
+        assert not multiple
+
+    return entities
+
+def get_entity(key):
+    entities = get_entities([key])
+    if len(entities) == 0:
+        return None
+    return entities[0]
+
+def put_entities(entities):
+    rpc = datastore.GetRpcFromKwargs({})
+    keys = datastore.Put(entities, rpc=rpc)
+    return keys
+
+def put_entity(entity):
+    return put_entities([entity])
 
 def yawp(path):
     # public
+    def delete_all(self, page_size=100):
+        q = self.query()
+        cursor = None
+        while True:
+            keys, cursor, more = q.fetch_page(page_size, keys_only=True, start_cursor=cursor)
+            ndb.delete_multi(keys)
+            if not more:
+                break
+
     def migrate(self, page_size, fn_str, *args, **keywords):
         fn = get_fn(fn_str, *args, **keywords)
 
@@ -21,16 +57,12 @@ def yawp(path):
         cursor = None
         while True:
             keys, cursor, more = q.fetch_page(page_size, keys_only=True, start_cursor=cursor)
-            entities = get_entities([key.to_old_key() for key in keys])
+            entities = get_entities(keys)
             for entity in entities:
                 fn(entity)
             put_entities(entities)
             if not more:
                 break
-
-    def put():
-        print 'disabled'
-
     # private
     def get_fn(fn_str, *args, **keywords):
         possibles = globals().copy()
@@ -38,27 +70,13 @@ def yawp(path):
         fn = possibles.get(fn_str)
         return partial(fn, *args, **keywords)
 
-    def get_entities(keys):
-        rpc = datastore.GetRpcFromKwargs({})
-        keys, multiple = datastore.NormalizeAndTypeCheckKeys(keys)
-        entities = None
-        try:
-            entities = datastore.Get(keys, rpc=rpc)
-        except datastore_errors.EntityNotFoundError:
-            assert not multiple
-
-        return entities
-
-    def put_entities(entities):
-        rpc = datastore.GetRpcFromKwargs({})
-        keys = datastore.Put(entities, rpc=rpc)
-        return keys
 
     # ndb model mapping
     def _get_kind(cls):
         return path
 
-    Model = type(path, (ndb.Model,), {'put':put})
+    Model = type(path, (ndb.Expando,), {})
     Model._get_kind = classmethod(_get_kind)
+    Model.delete_all = classmethod(delete_all)
     Model.migrate = classmethod(migrate)
     return Model

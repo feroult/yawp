ef163dc036ac918475d03538e7ea798a229a3420
feroult <feroult@gmail.com>
2015-07-01 21:37:30 -0300

    shields

---
+++ src/test/java/io/yawp/repository/actions/ShieldedObjectAction.java
@@ -0,0 +1,14 @@
+package io.yawp.repository.actions;
+
+import io.yawp.repository.IdRef;
+import io.yawp.repository.actions.annotations.PUT;
+import io.yawp.repository.models.basic.ShieldedObject;
+
+public class ShieldedObjectAction extends Action<ShieldedObject> {
+
+	@PUT("something")
+	public String something(IdRef<ShieldedObject> id) {
+		return "x";
+	}
+
+}

28d860c278b212f7fb9f9a6cde10f502e49a48d3
feroult <feroult@gmail.com>
2015-07-26 15:59:11 -0300

    full where

---
+++ src/main/java/io/yawp/repository/query/condition/AncestorConditionForChildException.java
@@ -0,0 +1,7 @@
+package io.yawp.repository.query.condition;
+
+public class AncestorConditionForChildException extends Exception {
+
+	private static final long serialVersionUID = -7758984382346177457L;
+
+}

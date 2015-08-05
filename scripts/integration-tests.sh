0026c5cb29075b5ac7fb5ed6825d9c75fcd457f6
feroult <feroult@gmail.com>
2015-08-05 09:49:40 -0300

    ci

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$JAVA_HOME" = "/usr/lib/jvm/java-7-oracle/bin/java" ]; then
-  echo "Shippable bug detected!"
-  export JAVA_HOME="/usr/lib/jvm/java-7-oracle"
-fi
+# if [ "$JAVA_HOME" = "/usr/lib/jvm/java-7-oracle/bin/java" ]; then
+#   echo "Shippable bug detected!"
+#   export JAVA_HOME="/usr/lib/jvm/java-7-oracle"
+# fi
 
 MAVEN_ARGS="-Dmaven.test.skip=true -Dappengine.port=8081 -Dappengine.jvmFlags.datastore.default_high_rep_job_policy_unapplied_job_pct=0"
 
@@ -11,8 +11,9 @@ echo "starting devserver..."
 (cd ..; mvn appengine:devserver_start $MAVEN_ARGS)
 echo "done."
 
+echo "running: $PWD"
 #./runner.py ../src/test/webapp/test/all.html
-phantomjs runner.js http://localhost:8081/test/all.html
+../phantomjs runner.js http://127.0.0.1:8081/test/all.html
 STATUS=$?
 
 echo "stopping devserver..."

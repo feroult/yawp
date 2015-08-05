7980834fd5c7cf11225276b1ebb515c5212f537b
feroult <feroult@gmail.com>
2015-08-05 10:21:27 -0300

    ci

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -6,20 +6,14 @@
 # fi
 
 MAVEN_ARGS="-Dmaven.test.skip=true -Dappengine.port=8081 -Dappengine.jvmFlags.datastore.default_high_rep_job_policy_unapplied_job_pct=0"
-
-echo "opts: $MAVEN_OPTS"
 unset MAVEN_OPTS
-echo "opts: $MAVEN_OPTS"
 
 echo "starting devserver..."
 (cd ..; mvn appengine:devserver_start $MAVEN_ARGS)
 echo "done."
 
-echo "running: $PWD"
-#./runner.py ../src/test/webapp/test/all.html
-wget http://localhost:8081/test/all.html
-cat all.html
-phantomjs runner.js http://localhost:8081/test/all.html
+./runner.py ../src/test/webapp/test/all.html
+#phantomjs runner.js http://localhost:8081/test/all.html
 STATUS=$?
 
 echo "stopping devserver..."

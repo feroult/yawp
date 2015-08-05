995ce6ead4ab116d7c59b021e362e2e795fd71fe
feroult <feroult@gmail.com>
2015-08-05 09:39:51 -0300

    ci

--- scripts/runner.js
+++ scripts/runner.js
@@ -492,11 +492,12 @@ console output.
         phantom.exit(1);
     };
 
-    page.onResourceRequested = function(request) {
-  console.log('Request ' + JSON.stringify(request, undefined, 4));
-};
-page.onResourceReceived = function(response) {
-  console.log('Receive ' + JSON.stringify(response, undefined, 4));
-};
+    // page.onResourceRequested = function(request) {
+    //     console.log('Request ' + JSON.stringify(request, undefined, 4));
+    // };
+    //
+    // page.onResourceReceived = function(response) {
+    //     console.log('Receive ' + JSON.stringify(response, undefined, 4));
+    // };
 
 }());

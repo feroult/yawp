06eb62386aa32bb677b389be32a7a2e82d50626d
feroult <feroult@gmail.com>
2015-09-12 22:16:50 -0300

    core/appegine modules split

---
+++ yawp-appengine/src/main/java/io/yawp/driver/appengine/AppengineEnvironmentDriver.java
@@ -0,0 +1,32 @@
+package io.yawp.driver.appengine;
+
+import io.yawp.driver.api.EnvironmentDriver;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.utils.SystemProperty;
+
+public class AppengineEnvironmentDriver implements EnvironmentDriver {
+
+	@Override
+	public boolean isProduction() {
+		return SystemProperty.environment.value() == SystemProperty.Environment.Value.Production;
+	}
+
+	@Override
+	public boolean isDevelopment() {
+		return SystemProperty.environment.value() == SystemProperty.Environment.Value.Development;
+	}
+
+	@Override
+	public boolean isTest() {
+		return !isProduction() && !isDevelopment();
+	}
+
+	@Override
+	public boolean isAdmin() {
+		UserService userService = UserServiceFactory.getUserService();
+		return userService.isUserLoggedIn() && userService.isUserAdmin();
+	}
+
+}
---
+++ yawp-appengine/src/main/java/io/yawp/driver/appengine/AppengineTransationDriver.java
@@ -0,0 +1,48 @@
+package io.yawp.driver.appengine;
+
+import io.yawp.driver.api.TransactionDriver;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.datastore.TransactionOptions;
+
+public class AppengineTransationDriver implements TransactionDriver {
+
+	private Transaction tx;
+
+	private DatastoreService datastore() {
+		return DatastoreServiceFactory.getDatastoreService();
+	}
+
+	@Override
+	public TransactionDriver begin() {
+		tx = datastore().beginTransaction();
+		return this;
+	}
+
+	@Override
+	public TransactionDriver beginX() {
+		TransactionOptions options = TransactionOptions.Builder.withXG(true);
+		tx = datastore().beginTransaction(options);
+		return this;
+	}
+
+	@Override
+	public void rollback() {
+		if (!tx.isActive()) {
+			tx = null;
+			return;
+		}
+
+		tx.rollback();
+		tx = null;
+	}
+
+	@Override
+	public void commit() {
+		tx.commit();
+		tx = null;
+	}
+
+}

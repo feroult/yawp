0ffa8ff5d0a5bacaf4738a5e08c0358cfe0134fc
feroult <feroult@gmail.com>
2015-10-13 21:49:41 -0300

    devserver plugin

--- yawp-maven-plugin/src/main/java/io/yawp/plugin/PluginAbstractMojo.java
+++ yawp-maven-plugin/src/main/java/io/yawp/plugin/PluginAbstractMojo.java
@@ -33,70 +33,24 @@ public abstract class PluginAbstractMojo extends AbstractMojo {
 	@Parameter(defaultValue = "${project}")
 	protected MavenProject project;
 
-	protected URLClassLoader createClassLoader() {
-		return createClassLoader(new ArrayList<String>());
-	}
-
-	protected URLClassLoader createClassLoader(List<String> customClasspathElements) {
-		try {
-			List<URL> urls = new ArrayList<URL>();
-
-			addURLs(project.getRuntimeClasspathElements(), urls);
-			addURLs(customClasspathElements, urls);
-
-			return new URLClassLoader(urls.toArray(new URL[] {}), Thread.currentThread().getContextClassLoader());
-		} catch (DependencyResolutionRequiredException | MalformedURLException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	protected void addURLs(List<String> classpathElements, List<URL> runtimeUrls) throws MalformedURLException {
-		List<String> elements = classpathElements;
-		for (int i = 0; i < elements.size(); i++) {
-			String element = (String) elements.get(i);
-			runtimeUrls.add(new File(element).toURI().toURL());
-			getLog().debug("Adding to webapp classpath: " + element);
-		}
-	}
-
 	public RepositorySystem getRepoSystem() {
 		return repoSystem;
-	}
-
-	protected void setRepoSystem(RepositorySystem repoSystem) {
-		this.repoSystem = repoSystem;
 	}
 
 	public RepositorySystemSession getRepoSession() {
 		return repoSession;
 	}
 
-	protected void setRepoSession(RepositorySystemSession repoSession) {
-		this.repoSession = repoSession;
-	}
-
 	public List<RemoteRepository> getProjectRepos() {
 		return projectRepos;
-	}
-
-	protected void setProjectRepos(List<RemoteRepository> projectRepos) {
-		this.projectRepos = projectRepos;
 	}
 
 	public List<RemoteRepository> getPluginRepos() {
 		return pluginRepos;
 	}
 
-	protected void setPluginRepos(List<RemoteRepository> pluginRepos) {
-		this.pluginRepos = pluginRepos;
-	}
-
 	public MavenProject getProject() {
 		return project;
-	}
-
-	protected void setProject(MavenProject project) {
-		this.project = project;
 	}
 
 }

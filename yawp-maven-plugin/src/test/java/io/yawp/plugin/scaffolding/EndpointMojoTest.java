72ed0a0353f08cb7f468b8800be350e436b7ce4d
feroult <feroult@gmail.com>
2015-11-14 21:18:00 -0200

    scaffolding

--- yawp-maven-plugin/src/test/java/io/yawp/plugin/scaffolding/EndpointMojoTest.java
+++ yawp-maven-plugin/src/test/java/io/yawp/plugin/scaffolding/EndpointMojoTest.java
@@ -1,44 +1,12 @@
 package io.yawp.plugin.scaffolding;
 
-import java.io.File;
-
-import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-
-public class EndpointMojoTest extends AbstractMojoTestCase {
-
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		deleteGeneratedFiles();
-	}
-
-	private void deleteGeneratedFiles() {
-		getModelFile().delete();
-		getModelTestFile().delete();
-	}
-
-	@Override
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+public class EndpointMojoTest extends ScaffolderMojoTestCase {
 
 	public void testCreateEndpoint() throws Exception {
-		File pom = getTestFile("src/test/resources/pom.xml");
+		executeGoal("endpoint");
 
-		EndpointMojo endpointMojo = (EndpointMojo) lookupMojo("endpoint", pom);
-		assertNotNull(endpointMojo);
-		endpointMojo.execute();
-
-		assertTrue(getModelFile().exists());
-		assertTrue(getModelTestFile().exists());
-	}
-
-	private File getModelFile() {
-		return new File("./target/scaffolding-test/src/main/java/yawpapp/models/person/Person.java");
-	}
-
-	private File getModelTestFile() {
-		return new File("./target/scaffolding-test/src/test/java/yawpapp/models/person/PersonTest.java");
+		assertSourceMain("/person/Person.java", "@Endpoint(path = \"/people\")");
+		assertSourceTest("/person/PersonTest.java", "public class PersonTest extends EndpointTestCase");
 	}
 
 }
---
+++ yawp-maven-plugin/src/test/java/io/yawp/plugin/scaffolding/ScaffolderMojoTestCase.java
@@ -0,0 +1,58 @@
+package io.yawp.plugin.scaffolding;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+public class ScaffolderMojoTestCase extends AbstractMojoTestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		FileUtils.deleteDirectory(new File("./target/scaffolding-test"));
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	protected void executeGoal(String goal) throws Exception, MojoExecutionException {
+		File pom = getTestFile("src/test/resources/pom.xml");
+		lookupMojo(goal, pom).execute();
+	}
+
+	protected void assertSourceTest(String filename, String content) {
+		File file = getSourceTest(filename);
+		assertTrue(file.exists());
+		assertFileContains(file, content);
+	}
+
+	protected void assertSourceMain(String filename, String content) {
+		File file = getSourceMain(filename);
+		assertTrue(file.exists());
+		assertFileContains(file, content);
+	}
+
+	private File getSourceMain(String filename) {
+		return new File("./target/scaffolding-test/src/main/java/yawpapp/models" + filename);
+	}
+
+	private File getSourceTest(String filename) {
+		return new File("./target/scaffolding-test/src/test/java/yawpapp/models" + filename);
+	}
+
+	private void assertFileContains(File file, String content) {
+		try {
+			String fileContent = new String(Files.readAllBytes(file.toPath()));
+			assertTrue(fileContent.indexOf(content) != -1);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}

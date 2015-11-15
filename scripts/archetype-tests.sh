2e4c55b4f9cd04097e93d1ee314cb627d49e56b5
feroult <feroult@gmail.com>
2015-11-15 13:20:45 -0200

    archetype tests

--- scripts/archetype-tests.sh
+++ scripts/archetype-tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 if [ $# -ne 1 ]; then
     echo "use: archetype-tests.sh YAWP_VERSION"
@@ -7,12 +7,14 @@ fi
 
 VERSION=$1
 WORK_DIR=.working/archetype-tests
+MAVEN_ARGS="-Dyawp.port=8081 -Dyawp.shutdownPort=8331"
+unset MAVEN_OPTS
 
 before() {
+    rm -rf $WORK_DIR
     mkdir -p $WORK_DIR
-    cd $CD_WORK_DIR
-    pwd
-    exit 1
+    cd $WORK_DIR
+    echo "generating project archetype to $WORK_DIR"
 
     mvn archetype:generate \
        -DarchetypeGroupId=io.yawp \
@@ -28,17 +30,44 @@ before() {
 
 after() {
     cd ../..
-    #rm -rf $WORK_DIR
+    rm -rf $WORK_DIR
 }
 
-run() {
+run_scaffolds() {
+    echo "runnning scaffolds..."
+
     mvn yawp:endpoint -Dmodel=person
     mvn yawp:action -Dmodel=person -Dname=dummy
     mvn yawp:transformer -Dmodel=person -Dname=dummy
     mvn yawp:hook -Dmodel=person -Dname=dummy
 
     mvn clean install
-    ls target/*.bin
+}
+
+run_endpoint_test() {
+    echo "stopping devserver..."
+    mvn yawp:devserver_stop $MAVEN_ARGS
+    echo "done."
+
+    echo "starting devserver..."
+    mvn yawp:devserver $MAVEN_ARGS &
+    mvn yawp:devserver_wait $MAVEN_ARGS
+    echo "done."
+
+    echo "calling endpoint"
+    curl -H "Content-type: application/json" -X POST \
+         -d "{'name': 'John'}" http://localhost:8081/api/people; echo
+
+    echo "stopping devserver..."
+    mvn yawp:devserver_stop $MAVEN_ARGS
+    echo "done."
+
+    ls target/appengine-generated/local_db.bin
+}
+
+run() {
+    run_scaffolds
+    run_endpoint_test
 }
 
 before

c041d1c1db6024f361996b159ec459e07c623c7a
feroult <feroult@gmail.com>
2015-11-15 15:23:56 -0200

    smoke tests

--- scripts/complete-it.sh
+++ scripts/complete-it.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
+
+VERSION=$(cat pom.xml | grep "<version>" | head -1 | cut -d ">" -f 2 | cut -d "<" -f 1)
+
+echo "running complete yawp it tests on version: $VERSION"
+
 mvn clean install -P it
-./scripts/scaffolding-it-test.sh
+./scripts/scaffolding-it-test.sh $VERSION

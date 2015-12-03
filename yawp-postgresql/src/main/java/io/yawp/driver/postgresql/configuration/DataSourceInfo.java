61af2fd0b3c68e5a6530dc4a6303fbf46db5af0f
feroult <feroult@gmail.com>
2015-12-03 10:15:47 -0200

    postgres configuration

---
+++ yawp-postgresql/src/main/java/io/yawp/driver/postgresql/configuration/DataSourceInfo.java
@@ -0,0 +1,45 @@
+package io.yawp.driver.postgresql.configuration;
+
+public class DataSourceInfo {
+
+    private String name;
+
+    private String driverClassName;
+
+    private String url;
+
+    private String env;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getEnv() {
+        return env;
+    }
+
+    public void setEnv(String env) {
+        this.env = env;
+    }
+}

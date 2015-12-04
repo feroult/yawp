72122896980388c0658fa5ea01f87a118a31a760
feroult <feroult@gmail.com>
2015-12-04 17:17:31 -0200

    endpoint path format tests

---
+++ yawp-core/src/test/java/io/yawp/repository/RepositoryFeaturesTest.java
@@ -0,0 +1,27 @@
+package io.yawp.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RepositoryFeaturesTest {
+
+    @Test
+    public void testValidEndpointPaths() {
+        RepositoryFeatures features = new RepositoryFeatures();
+
+        assertTrue(features.isValidEndpointPath("/parents"));
+        assertTrue(features.isValidEndpointPath("/par_ents"));
+        assertTrue(features.isValidEndpointPath("/par-ents"));
+    }
+
+    @Test
+    public void testInvalidEndpointPaths() {
+        RepositoryFeatures features = new RepositoryFeatures();
+
+        assertFalse(features.isValidEndpointPath("parents"));
+        assertFalse(features.isValidEndpointPath("/123parents"));
+    }
+
+}

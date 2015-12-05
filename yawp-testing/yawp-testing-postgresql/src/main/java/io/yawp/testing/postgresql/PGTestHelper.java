8d0831f6fabddb9bccee29f41f82b79064cf4923
feroult <feroult@gmail.com>
2015-12-05 15:15:31 -0200

    pg sync

--- yawp-testing/yawp-testing-postgresql/src/main/java/io/yawp/testing/postgresql/PGTestHelper.java
+++ yawp-testing/yawp-testing-postgresql/src/main/java/io/yawp/testing/postgresql/PGTestHelper.java
@@ -1,13 +1,18 @@
 package io.yawp.testing.postgresql;
 
 import io.yawp.commons.utils.Environment;
+import io.yawp.commons.utils.ResourceFinder;
 import io.yawp.driver.api.testing.TestHelper;
 import io.yawp.driver.postgresql.configuration.InitialContextSetup;
 import io.yawp.driver.postgresql.datastore.SchemaSynchronizer;
 import io.yawp.repository.Repository;
 
+import java.io.File;
+import java.io.IOException;
+
 public class PGTestHelper implements TestHelper {
 
+    public static final String INTERNAL_TEST_JETTY_ENV_XML = "configuration/jetty-env.xml";
     private Repository r;
 
     @Override
@@ -19,7 +24,11 @@ public class PGTestHelper implements TestHelper {
 
     private void configureInitialContext() {
         Environment.set("test");
-        InitialContextSetup.configure("configuration/jetty-env.xml");
+        if (isUserTest()) {
+            InitialContextSetup.configure(getUserJettyConfigFile());
+        } else {
+            InitialContextSetup.configure(INTERNAL_TEST_JETTY_ENV_XML);
+        }
     }
 
     private void resetTables() {
@@ -34,6 +43,26 @@ public class PGTestHelper implements TestHelper {
 
     @Override
     public void tearDown() {
+    }
+
+    public boolean isUserTest() {
+        try {
+            new ResourceFinder().find(INTERNAL_TEST_JETTY_ENV_XML);
+        } catch (IOException e) {
+            return true;
+        }
+        return false;
+    }
+
+    private File getUserJettyConfigFile() {
+        return new File(String.format("%s/src/main/webapp/WEB-INF/jetty-env.xml", getBaseDir()));
+    }
+
+    private String getBaseDir() {
+        if(Environment.getBaseDir() != null) {
+            return Environment.getBaseDir();
+        }
+        return PGTestHelper.class.getResource("/").getFile() + "../../";
     }
 
 }

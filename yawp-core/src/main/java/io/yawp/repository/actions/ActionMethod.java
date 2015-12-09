311e0b9c726435fc05e76f46e089909974e92c43
feroult <feroult@gmail.com>
2015-12-09 00:15:13 -0200

    action methods with json support

--- yawp-core/src/main/java/io/yawp/repository/actions/ActionMethod.java
+++ yawp-core/src/main/java/io/yawp/repository/actions/ActionMethod.java
@@ -1,13 +1,9 @@
 package io.yawp.repository.actions;
 
 import io.yawp.commons.http.HttpVerb;
-import io.yawp.commons.utils.ReflectionUtils;
 import io.yawp.repository.IdRef;
-import io.yawp.repository.ObjectModel;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,28 +12,14 @@ public class ActionMethod {
 
     private Method method;
 
-    private Class<?> endpointClazz;
-
-    private Type[] parameterTypes;
-
-    private final ObjectModel objectModel;
-
-    private Type[] genericParameterTypes;
-
     private ActionParameters parameters;
 
     private final List<ActionKey> actionKeys;
 
     public ActionMethod(Method method) throws InvalidActionMethodException {
-        this.parameters = new ActionParameters(method);
-
         this.method = method;
-        this.endpointClazz = ReflectionUtils.getGenericParameter(method.getDeclaringClass());
-        this.parameterTypes = method.getParameterTypes();
-        this.genericParameterTypes = method.getGenericParameterTypes();
-        this.objectModel = new ObjectModel(endpointClazz);
+        this.parameters = new ActionParameters(method);
         this.actionKeys = parseActionKeys();
-
     }
 
     public List<ActionKey> getActionKeys() {
@@ -78,16 +60,6 @@ public class ActionMethod {
 
     public Object[] createArguments(IdRef<?> id, Map<String, String> params) {
         return parameters.createArguments(id, params);
-
-    }
-
-
-    private boolean isParamaterTypeOf(int index, Class<?> clazz) {
-        return parameterTypes[index].equals(clazz);
-    }
-
-    private int parameterCount() {
-        return parameterTypes.length;
     }
 
 }

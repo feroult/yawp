3f3f00aa98ffc255f36cc8d189cdf6f3298d462e
feroult <feroult@gmail.com>
2015-12-17 13:54:20 -0200

    snapshot releases script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
 VERSION=$1
 echo "releasing: $VERSION"
 mvn versions:set -DnewVersion=$VERSION
 mvn versions:commit
 git commit -am "release $VERSION"
-# check if it is not a snapshot
-git tag "yawp-$VERSION"
-git push --tags
+
+if [[ ! $VERSION =~ .*SNAPSHOT.* ]]; then
+    git tag "yawp-$VERSION"
+    git push --tags
+fi
+
 git push
 killall -9 gpg-agent
 eval "$(gpg-agent --daemon)"

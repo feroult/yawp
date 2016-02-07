514e8e9b83f52b91ebb34bbbe308db48eece80a5
feroult <feroult@gmail.com>
2016-02-07 16:14:27 -0200

    pipes

---
+++ yawp-core/src/test/java/io/yawp/repository/models/basic/PipedObject.java
@@ -0,0 +1,30 @@
+package io.yawp.repository.models.basic;
+
+import io.yawp.repository.IdRef;
+import io.yawp.repository.annotations.Endpoint;
+import io.yawp.repository.annotations.Id;
+
+@Endpoint(path = "/piped_objects")
+public class PipedObject {
+
+    @Id
+    private IdRef<PipedObject> id;
+
+    private String group;
+
+    public IdRef<PipedObject> getId() {
+        return id;
+    }
+
+    public void setId(IdRef<PipedObject> id) {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}

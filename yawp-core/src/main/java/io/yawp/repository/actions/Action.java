6014cd64f279af61c723a6cbc8b8c049e06b295f
feroult <feroult@gmail.com>
2016-02-18 14:49:12 -0200

    some javadoc

--- yawp-core/src/main/java/io/yawp/repository/actions/Action.java
+++ yawp-core/src/main/java/io/yawp/repository/actions/Action.java
@@ -3,12 +3,28 @@ package io.yawp.repository.actions;
 import io.yawp.commons.utils.JsonUtils;
 import io.yawp.repository.Feature;
 
+
+/**
+ * Action API
+ * <p/>
+ * The Action API is used to create custom rest actions to manipulate
+ * or retrieve the state of a group of endpoint models.
+ * <p/>
+ * All subclasses of this base class will be scanned at the bootstrap
+ * of the framework.
+ * <p/>
+ * All the public methods annotated with one of the http-verb annotations
+ * (GET, POST, PUT, PATCH, DELETE) will be assigned to custom rest routes
+ * bellow the path of the parametrized endpoint model.
+ *
+ * @param <T> The endpoint model type.
+ */
 public class Action<T> extends Feature {
 
-    public <T> T from(String json, Class<T> clazz) {
-        return JsonUtils.from(yawp, json, clazz);
-    }
-
+    /**
+     * @deprecated use {@link Feature#to(Object)}.
+     */
+    @Deprecated
     public String json(Object object) {
         return JsonUtils.to(object);
     }

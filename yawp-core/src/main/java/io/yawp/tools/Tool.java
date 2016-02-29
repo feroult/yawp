3152cfe02fa88316605511c4dbfad6413352e906
feroult <feroult@gmail.com>
2016-02-28 21:48:06 -0300

    pipes

---
+++ yawp-core/src/main/java/io/yawp/tools/Tool.java
@@ -0,0 +1,37 @@
+package io.yawp.tools;
+
+import io.yawp.repository.Feature;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+public abstract class Tool extends Feature {
+
+    private static final String NS_PARAM = "ns";
+
+    protected Map<String, String> params;
+
+    protected PrintWriter pw;
+
+    public abstract void execute();
+
+    public void prepareAndExecute() throws IOException {
+        init();
+        setNamespace();
+        execute();
+    }
+
+    private void init() throws IOException {
+        this.params = requestContext.getParams();
+        this.pw = requestContext.resp().getWriter();
+    }
+
+    private void setNamespace() {
+        String ns = params.get(NS_PARAM);
+        if (ns != null) {
+            yawp.namespace(ns);
+        }
+    }
+
+}

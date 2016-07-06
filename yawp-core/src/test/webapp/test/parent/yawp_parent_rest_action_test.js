373254644fd0c1eaafb76b16e1c831e040bf4ca4
feroult <feroult@gmail.com>
2016-07-06 18:58:49 -0300

    client

--- yawp-core/src/test/webapp/test/parent/yawp_parent_rest_action_test.js
+++ yawp-core/src/test/webapp/test/parent/yawp_parent_rest_action_test.js
@@ -1,37 +1,37 @@
-(function(t, yawp, fx) {
+(function (t, yawp, fx) {
 
-	t.moduledef('parent rest action', {
-		testStart : function(start) {
-			fx.reset().then(start);
-		}
-	});
+    t.moduledef('parent rest action', {
+        testStart: function (start) {
+            fx.reset().then(start);
+        }
+    });
 
-	t.asyncTest("create", function(assert) {
-		expect(1);
+    t.asyncTest("create", function (assert) {
+        expect(1);
 
-		var parent = {
-			name : 'xpto'
-		};
+        var parent = {
+            name: 'xpto'
+        };
 
-		yawp('/parents').create(parent).then(function(retrievedParent) {
-			assert.equal(retrievedParent.name, 'xpto');
-			t.start();
-		});
-	});
+        yawp('/parents').create(parent).then(function (retrievedParent) {
+            assert.equal(retrievedParent.name, 'xpto');
+            t.start();
+        });
+    });
 
-    t.asyncTest("create with lazy json", function(assert) {
+    t.asyncTest("create with lazy json", function (assert) {
         expect(4);
 
         var parent = {
-            name : 'xpto',
-            job : {
+            name: 'xpto',
+            job: {
                 name: 'job xpto1'
             },
-			jobs: [ { name: 'job xpto2' }],
-            jobsMap: { '/jobs/1': { name: 'job xpto3' } }
+            jobs: [{name: 'job xpto2'}],
+            jobsMap: {'/jobs/1': {name: 'job xpto3'}}
         };
 
-        yawp('/parents').create(parent).done(function(retrievedParent) {
+        yawp('/parents').create(parent).then(function (retrievedParent) {
             assert.equal(retrievedParent.name, 'xpto');
             assert.equal(retrievedParent.job.name, 'job xpto1');
             assert.equal(retrievedParent.jobs[0].name, 'job xpto2');
@@ -40,140 +40,154 @@
         });
     });
 
-    t.asyncTest("create with lazy json and array", function(assert) {
+    t.asyncTest("create with lazy json and array", function (assert) {
         expect(3);
 
-        yawp('/jobs').create({name : 'job xpto'}).done(function(job){
-        	var parent = {
-            	name : 'xpto',
-	            job : {
-	                name: 'job xpto'
-	            },
-	            pastJobIds : [job.id]
-	        };
+        yawp('/jobs').create({name: 'job xpto'}).then(function (job) {
+            var parent = {
+                name: 'xpto',
+                job: {
+                    name: 'job xpto'
+                },
+                pastJobIds: [job.id]
+            };
 
-	        yawp('/parents').create(parent).done(function(retrievedParent) {
-	            assert.equal(retrievedParent.name, 'xpto');
-	            assert.equal(retrievedParent.job.name, 'job xpto');
+            yawp('/parents').create(parent).then(function (retrievedParent) {
+                assert.equal(retrievedParent.name, 'xpto');
+                assert.equal(retrievedParent.job.name, 'job xpto');
                 assert.equal(retrievedParent.pastJobIds[0], job.id);
-	            t.start();
-	        });
+                t.start();
+            });
         });
     });
 
-	t.asyncTest("create array", function(assert) {
-		expect(3);
+    t.asyncTest("create array", function (assert) {
+        expect(3);
 
-		var parents = [ {
-			name : 'xpto1'
-		}, {
-			name : 'xpto2'
-		} ];
+        var parents = [{
+            name: 'xpto1'
+        }, {
+            name: 'xpto2'
+        }];
 
-		yawp('/parents').create(parents).done(function(retrievedParents) {
-			assert.equal(retrievedParents.length, 2)
-			assert.equal(retrievedParents[0].name, 'xpto1');
-			assert.equal(retrievedParents[1].name, 'xpto2');
-			t.start();
-		});
-	});
+        yawp('/parents').create(parents).then(function (retrievedParents) {
+            assert.equal(retrievedParents.length, 2)
+            assert.equal(retrievedParents[0].name, 'xpto1');
+            assert.equal(retrievedParents[1].name, 'xpto2');
+            t.start();
+        });
+    });
 
-	t.asyncTest("update", function(assert) {
-		expect(1);
+    t.asyncTest("update", function (assert) {
+        expect(1);
 
-		var parent = fx.parent('parent', {
-			name : 'xpto'
-		});
+        fx.parent('parent', {
+            name: 'xpto'
+        });
 
-		parent.name = 'changed xpto';
-		yawp.update(parent).done(function(retrievedParent) {
-			assert.equal(retrievedParent.name, 'changed xpto');
-			t.start();
-		});
-	});
+        fx.load(function () {
+            var parent = fx.parent.parent;
+            parent.name = 'changed xpto';
 
-	t.asyncTest("patch", function(assert) {
-		expect(2);
+            yawp.update(parent).then(function (retrievedParent) {
+                assert.equal(retrievedParent.name, 'changed xpto');
+                t.start();
+            });
+        });
+    });
 
-		var job = fx.job('coder', {
-			name : 'coder'
-		});
+    t.asyncTest("patch", function (assert) {
+        expect(2);
 
-		var parent = fx.parent('parent', {
-			name : 'xpto',
-			jobId : job.id
-		});
+        fx.job('coder', {
+            name: 'coder'
+        });
 
-		var parentPatch = {
-			id : parent.id,
-			name : 'changed xpto'
-		};
+        fx.parent('parent', {
+            name: 'xpto',
+            jobId: fx.job.coder.id
+        });
 
-		yawp.patch(parentPatch).done(function(retrievedParent) {
-			assert.equal(retrievedParent.name, 'changed xpto');
-			assert.equal(retrievedParent.jobId, job.id);
-			t.start();
-		});
-	});
+        fx.load(function () {
+            var parentPatch = {
+                id: fx.parent.parent.id,
+                name: 'changed xpto'
+            };
 
-	t.asyncTest("show", function(assert) {
-		expect(1);
+            yawp.patch(parentPatch).then(function (retrievedParent) {
+                assert.equal(retrievedParent.name, 'changed xpto');
+                assert.equal(retrievedParent.jobId, fx.job.coder.id);
+                t.start();
+            });
+        });
+    });
 
-		var parent = fx.parent('parent', {
-			name : 'xpto'
-		});
+    t.asyncTest("show", function (assert) {
+        expect(1);
 
-		yawp(parent).fetch(function(retrievedParent) {
-			assert.equal(retrievedParent.name, 'xpto');
-			t.start();
-		});
+        fx.parent('parent', {
+            name: 'xpto'
+        });
 
-	});
+        fx.load(function () {
+            var parent = fx.parent.parent;
 
-	t.asyncTest("index", function(assert) {
-		expect(3);
+            yawp(parent).fetch(function (retrievedParent) {
+                assert.equal(retrievedParent.name, 'xpto');
+                t.start();
+            });
+        });
 
-		fx.parent('parent1', {
-			name : 'xpto1'
-		});
+    });
 
-		fx.parent('parent2', {
-			name : 'xpto2'
-		});
+    t.asyncTest("index", function (assert) {
+        expect(3);
 
-		var order = [ {
-			p : 'name'
-		} ];
+        fx.parent('parent1', {
+            name: 'xpto1'
+        });
 
-		yawp('/parents').order(order).list(function(parents) {
+        fx.parent('parent2', {
+            name: 'xpto2'
+        });
 
-			assert.equal(parents.length, 2);
-			assert.equal(parents[0].name, 'xpto1');
-			assert.equal(parents[1].name, 'xpto2');
-			t.start();
-		});
-	});
+        fx.load(function () {
+            var order = [{
+                p: 'name'
+            }];
 
-	t.asyncTest("destroy", function(assert) {
-		expect(2);
+            yawp('/parents').order(order).list(function (parents) {
+                assert.equal(parents.length, 2);
+                assert.equal(parents[0].name, 'xpto1');
+                assert.equal(parents[1].name, 'xpto2');
+                t.start();
+            });
+        });
+    });
 
-		var parent = fx.parent('parent', {
-			name : 'xpto'
-		});
+    t.asyncTest("destroy", function (assert) {
+        expect(2);
 
-		yawp.destroy(parent).done(function(retrievedId) {
-			t.equal(parent.id, retrievedId);
+        fx.parent('parent', {
+            name: 'xpto'
+        });
 
-		}).then(function() {
-			yawp(parent).fetch().fail(function(error) {
-				if (t.isPhantomJS()) {
-					assert.equal(error.status, 0);
-				} else {
-					assert.equal(error.status, 404);
-				}
-				t.start();
-			});
-		});
-	});
+        fx.load(function () {
+            var parent = fx.parent.parent;
+
+            yawp.destroy(parent).then(function (retrievedId) {
+                t.equal(parent.id, retrievedId);
+            }).then(function () {
+                yawp(parent).fetch().catch(function (error) {
+                    if (t.isPhantomJS()) {
+                        assert.equal(error.status, 0);
+                    } else {
+                        assert.equal(error.status, 404);
+                    }
+                    t.start();
+                });
+            });
+        });
+    });
 
 })(QUnit, yawp, yawp.fixtures);

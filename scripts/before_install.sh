7dc18ba6f3f3f60ce933c367ead33c33ee1fcf2f
feroult <feroult@gmail.com>
2016-07-07 22:28:04 -0300

    client

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,6 +12,7 @@ cd ..
 export PATH=$PWD/phantomjs-2.0.0:$PATH
 
 # node
+sudo apt-get remove nodejs -Y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 node --version

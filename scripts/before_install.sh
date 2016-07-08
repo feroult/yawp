1c39f2f00a8e1586fcd0e92991bae1e4679e9a77
feroult <feroult@gmail.com>
2016-07-07 22:36:08 -0300

    client

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -17,3 +17,6 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 which node
 node --version
+export PATH=/usr/local/bin/node:$PATH
+which node
+node --version

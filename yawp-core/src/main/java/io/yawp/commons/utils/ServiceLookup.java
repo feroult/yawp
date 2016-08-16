113c31d84e362fd81183c63985a40ca13dfd8600
feroult <feroult@gmail.com>
2016-08-15 21:55:12 -0300

    performance improvements

--- yawp-core/src/main/java/io/yawp/commons/utils/ServiceLookup.java
+++ yawp-core/src/main/java/io/yawp/commons/utils/ServiceLookup.java
@@ -1,34 +1,43 @@
 package io.yawp.commons.utils;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ServiceLookup {
 
     public static final String SERVICES_PATH = "META-INF/services/";
 
-    private ServiceLookup() {}
+    public static Map<Class<?>, Class<?>> cache = new HashMap<>();
+
+    private ServiceLookup() {
+    }
 
     public static <T> T lookup(Class<T> clazz) {
-        ResourceFinder finder = new ResourceFinder(SERVICES_PATH);
-        return lookup(finder, clazz);
-    }
-
-    public static <T> T lookup(Class<T> clazz, ClassLoader classLoader) {
-        ResourceFinder finder = new ResourceFinder(SERVICES_PATH, classLoader);
-        return lookup(finder, clazz);
-    }
-
-    private static <T> T lookup(ResourceFinder finder, Class<T> clazz) {
         try {
-            List<Class> clazzes = finder.findAllImplementations(clazz);
-
-            if(clazzes.size() == 0) {
-                throw new RuntimeException(String.format("No service implementation for %s.", clazz.getSimpleName()));
+            Class serviceClazz;
+            if (!cache.containsKey(clazz)) {
+                serviceClazz = findServiceClazz(clazz);
+                cache.put(clazz, serviceClazz);
+            } else {
+                serviceClazz = cache.get(clazz);
             }
-
-            return (T) clazzes.get(0).newInstance();
+            return (T) serviceClazz.newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    private static <T> Class findServiceClazz(Class<T> clazz) throws IOException, ClassNotFoundException {
+        ResourceFinder finder = new ResourceFinder(SERVICES_PATH);
+        List<Class> clazzes = finder.findAllImplementations(clazz);
+
+        if (clazzes.size() == 0) {
+            throw new RuntimeException(String.format("No service implementation for %s.", clazz.getSimpleName()));
+        }
+
+        return clazzes.get(0);
+    }
+
 }

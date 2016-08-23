e8b6db23279f54d434ed01b829b2bf709e9361d9
feroult <feroult@gmail.com>
2016-08-23 15:06:52 -0300

    genson

--- yawp-core/src/test/java/io/yawp/servlet/hierarchy/HierarchyShieldTest.java
+++ yawp-core/src/test/java/io/yawp/servlet/hierarchy/HierarchyShieldTest.java
@@ -7,14 +7,14 @@ public class HierarchyShieldTest extends ServletTestCase {
 
     @Test
     public void testSuperClassAction() {
-        assertPostWithStatus("/hierarchy_subclasses/1", "{ name: 'block this case' }", 404);
-        assertPostWithStatus("/hierarchy_subclasses/1", "{ name: 'block this case too' }", 200);
-        assertPostWithStatus("/hierarchy_another-subclasses/1", "{ name: 'block this case' }", 404);
+        assertPostWithStatus("/hierarchy_subclasses/1", "{ \"name\": \"block this case\" }", 404);
+        assertPostWithStatus("/hierarchy_subclasses/1", "{ \"name\": \"block this case too\" }", 200);
+        assertPostWithStatus("/hierarchy_another-subclasses/1", "{ \"name\": \"block this case\" }", 404);
     }
 
     @Test
     public void testMoreSpecificSubClassShield() {
-        assertPostWithStatus("/hierarchy_another-subclasses/1", "{ name: 'block this case too' }", 404);
+        assertPostWithStatus("/hierarchy_another-subclasses/1", "{ \"name\": \"block this case too\" }", 404);
     }
 
 }

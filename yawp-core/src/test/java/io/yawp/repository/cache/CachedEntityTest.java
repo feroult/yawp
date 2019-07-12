7e21c07086ea401f16c574351b4e586e15572ce0
Luan Nico <luanpotter27@gmail.com>
2019-07-12 18:50:01 -0300

    formatting

--- yawp-core/src/test/java/io/yawp/repository/cache/CachedEntityTest.java
+++ yawp-core/src/test/java/io/yawp/repository/cache/CachedEntityTest.java
@@ -15,48 +15,48 @@ import static org.hamcrest.core.Is.is;
 
 public class CachedEntityTest extends EndpointTestCase {
 
-	@Test
-	public void testCacheQueryViaList() {
-		yawp.save(new CachedEntity("foo"));
-		Map<String, Object> cacheList = CachedEntityHook.caches.get(QueryType.QUERY);
+    @Test
+    public void testCacheQueryViaList() {
+        yawp.save(new CachedEntity("foo"));
+        Map<String, Object> cacheList = CachedEntityHook.caches.get(QueryType.QUERY);
 
-		assertThat(cacheList.size(), is(equalTo(0)));
-		List<CachedEntity> list = yawp(CachedEntity.class).list();
-		assertThat(cacheList.size(), is(equalTo(1)));
-		assertThat(list.get(0).getText(), is(equalTo("foo")));
+        assertThat(cacheList.size(), is(equalTo(0)));
+        List<CachedEntity> list = yawp(CachedEntity.class).list();
+        assertThat(cacheList.size(), is(equalTo(1)));
+        assertThat(list.get(0).getText(), is(equalTo("foo")));
 
-		List<CachedEntity> list2 = yawp(CachedEntity.class).list();
-		assertThat(cacheList.size(), is(equalTo(1)));
-		assertSame(list.get(0), list2.get(0)); // from cache
-	}
+        List<CachedEntity> list2 = yawp(CachedEntity.class).list();
+        assertThat(cacheList.size(), is(equalTo(1)));
+        assertSame(list.get(0), list2.get(0)); // from cache
+    }
 
-	@Test
-	public void testCacheFetch() {
-		CachedEntity foo = yawp.save(new CachedEntity("foo"));
-		Map<String, Object> cacheFetch = CachedEntityHook.caches.get(QueryType.FETCH);
+    @Test
+    public void testCacheFetch() {
+        CachedEntity foo = yawp.save(new CachedEntity("foo"));
+        Map<String, Object> cacheFetch = CachedEntityHook.caches.get(QueryType.FETCH);
 
-		assertThat(cacheFetch.size(), is(equalTo(0)));
-		CachedEntity refetch = foo.getId().refetch();
-		assertThat(cacheFetch.size(), is(equalTo(1)));
-		assertThat(refetch.getText(), is(equalTo("foo")));
+        assertThat(cacheFetch.size(), is(equalTo(0)));
+        CachedEntity refetch = foo.getId().refetch();
+        assertThat(cacheFetch.size(), is(equalTo(1)));
+        assertThat(refetch.getText(), is(equalTo("foo")));
 
-		CachedEntity refetch2 = foo.getId().refetch();
-		assertThat(cacheFetch.size(), is(equalTo(1)));
-		assertSame(refetch, refetch2); // from cache
-	}
+        CachedEntity refetch2 = foo.getId().refetch();
+        assertThat(cacheFetch.size(), is(equalTo(1)));
+        assertSame(refetch, refetch2); // from cache
+    }
 
-	@Test
-	public void testCacheQueryViaIds() {
-		CachedEntity foo = yawp.save(new CachedEntity("foo"));
-		Map<String, Object> cacheIds = CachedEntityHook.caches.get(QueryType.QUERY);
+    @Test
+    public void testCacheQueryViaIds() {
+        CachedEntity foo = yawp.save(new CachedEntity("foo"));
+        Map<String, Object> cacheIds = CachedEntityHook.caches.get(QueryType.QUERY);
 
-		assertThat(cacheIds.size(), is(equalTo(0)));
-		List<IdRef<CachedEntity>> list = yawp(CachedEntity.class).ids();
-		assertThat(cacheIds.size(), is(equalTo(1)));
-		assertThat(list.get(0).toString(), is(equalTo(foo.getId().toString())));
+        assertThat(cacheIds.size(), is(equalTo(0)));
+        List<IdRef<CachedEntity>> list = yawp(CachedEntity.class).ids();
+        assertThat(cacheIds.size(), is(equalTo(1)));
+        assertThat(list.get(0).toString(), is(equalTo(foo.getId().toString())));
 
-		List<IdRef<CachedEntity>> list2 = yawp(CachedEntity.class).ids();
-		assertThat(cacheIds.size(), is(equalTo(1)));
-		assertSame(list, list2); // from cache
-	}
+        List<IdRef<CachedEntity>> list2 = yawp(CachedEntity.class).ids();
+        assertThat(cacheIds.size(), is(equalTo(1)));
+        assertSame(list, list2); // from cache
+    }
 }
